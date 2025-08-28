d3434f5bc3d3dd375fbee37f6e23877d46be2ac5
Emilse Arias <emilsearias@gmail.com>
2025-08-28 20:20:03 -0300

    agregamos un Navbar de react-bootstrap y agregamos iconos de bootstrap

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -1,9 +1,20 @@
+import {Navbar, Container, Nav} from 'react-bootstrap'
+
 const Menu = () => {
-    return (
-        <div>
-            <h2>Menú</h2>
-        </div>
-    );
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Menu;
