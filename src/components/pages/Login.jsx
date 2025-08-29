fc9e0edf44cb89fc61875a883c3ccb3d8797124e
Emilse Arias <emilsearias@gmail.com>
2025-08-29 11:30:32 -0300

    agregue estilos al login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -8,56 +8,66 @@ const Login = () => {
     formState: { errors },
   } = useForm();
 
-const onSubmit = (data) => {
-  console.log(data);
-//   agregar logica de login
-};
+  const onSubmit = (data) => {
+    console.log(data);
+    //   agregar logica de login
+  };
 
   return (
-    <Card>
-      <Row xs={1} md={2}>
-        <Col>
-          <Card.Body>
-            <Form onSubmit={handleSubmit(onSubmit)}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email:</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Ej: juanperez@mail.com"
-                  {...register('email',{
-                    required: 'El email es un dato obligatorio',
-                    pattern:{
-                        value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-                        message: 'El email debe ser un correo valido por ej: juanperez@mail.com'
-                    }
-                  })}
-                />
-                <Form.Text className="text-danger">{errors.email?.message}</Form.Text>
-              </Form.Group>
+    <>
+      <Card className=" shadow p-3 mb-5 bg-body rounded card-login">
+        <Row xs={1} md={2}>
+          <Col>
+            <Card.Body>
+              <h1 className="text-center mb-4">Iniciar sesion</h1>
+              {/* <h3>Ingresa los datos de tu</h3> */}
+              <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Ej: juanperez@mail.com"
+                    {...register("email", {
+                      required: "El email es un dato obligatorio",
+                      pattern: {
+                        value:
+                          /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                        message:
+                          "El email debe ser un correo valido por ej: juanperez@mail.com",
+                      },
+                    })}
+                  />
+                  <Form.Text className="text-danger">
+                    {errors.email?.message}
+                  </Form.Text>
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Contraseña:</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Ingresa una contraseña"
-                />
-                <Form.Text className="text-danger">mensaje de error </Form.Text>
-              </Form.Group>
-              <Button variant="warning" type="submit">
-                Iniciar sesión
-              </Button>
-            </Form>
-          </Card.Body>
-        </Col>
-        <Col>
-          <img
-            src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg"
-            alt="Imagen comida"
-            className="img-fluid"
-          />
-        </Col>
-      </Row>
-    </Card>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Contraseña:</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Ingresa una contraseña"
+                  />
+                  <Form.Text className="text-danger">
+                    
+                  </Form.Text>
+                </Form.Group>
+                <Button variant="warning" type="submit">
+                  Iniciar sesión
+                </Button>
+              </Form>
+            </Card.Body>
+          </Col>
+          <Col>
+            <img
+              src="https://images.pexels.com/photos/905847/pexels-photo-905847.jpeg"
+              alt="Imagen comida"
+              className="img-login rounded-3"
+            />
+          </Col>
+        </Row>
+      </Card>
+    </>
   );
 };
 
